713d5a1a0aa16f7005c105815dea784b57323808
rorubyy <roruby118@gmail.com>
2023-03-15 14:41:37 +0800

    add comment to leetcode0958

--- leetcode/medium/0958.Check-Completeness-of-a-Binary-Tree/leetcode0958.go
+++ leetcode/medium/0958.Check-Completeness-of-a-Binary-Tree/leetcode0958.go
@@ -5,16 +5,16 @@ import "leetcode-Go/structures"
 type TreeNode = structures.TreeNode
 
 func isCompleteTree(root *TreeNode) bool {
-	queue, found := []*TreeNode{root}, false
+	queue, found := []*TreeNode{root}, false //initialize a queue save tree node
 	for len(queue) > 0 {
 		node := queue[0]
-		queue = queue[1:]
+		queue = queue[1:] //pop the first node from queue
 		if node == nil {
 			found = true
 		} else {
 			if found {
 				return false
-			}
+			} // if find a node after a nil -> not a completed tree
 			queue = append(queue, node.Left, node.Right)
 		}
 	}
